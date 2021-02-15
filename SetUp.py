1f026cf99a038ff0f417607ce866a5ea988133d9
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-15 22:00:32 +0800

    Create SetUp.py

---
+++ SetUp.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='eia-python',
+    version='1.22',
+    packages=['eia'],
+    url='https://github.com/mra1385/eia-python',
+    license='BSD',
+    author='Mike Azar',
+    author_email='mikeazar@gmail.com',
+    description='Python Wrapper for the Energy Information Administration (eia) API'
+)
