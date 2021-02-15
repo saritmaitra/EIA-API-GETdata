057cdead2fd60d1d9e9cf65927905a555b168ac5
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-15 23:14:02 +0800

    Create example6.py

---
+++ example2/example6.py
@@ -0,0 +1,41 @@
+# U.S. Natural Gas Marketed Production, Monthly
+print('\033[4mNatural Gas Marketed Production, Monthly (Million Cubic Feet)\033[0m')
+def retrieve_time_series(api, series_ID):
+    """
+    Return the time series dataframe, based on API and unique Series ID
+    """
+    #Retrieve Data By Series ID 
+    series_search = api.data_by_series(series=series_ID)
+    ##Create a pandas dataframe from the retrieved time series
+    prod = pd.DataFrame(series_search)
+    return prod
+
+def main():
+    """
+    Run main script
+    """
+    try:
+      #Create EIA API using your specific API key
+      api_key = "ad819ee5a69e69390eadf300fa168fa8"
+      api = eia.API(api_key)
+      #Declare desired series ID
+      series_ID='NG.N9050US2.M'
+      prod = retrieve_time_series(api, series_ID)
+      #Print the returned dataframe df
+      print(type(prod))
+      return prod;
+    except Exception as e:
+      print("error", e)
+      return pd.DataFrame(columns=None)
+
+prod = main()
+prod = prod.rename({'U.S. Natural Gas Marketed Production, Monthly (Million Cubic Feet)': 'prod'}, axis = 'columns')
+prod = prod.reset_index()
+prod['Date']= pd.to_datetime(prod['index']) 
+prod.set_index('Date', inplace=True) # setting index column
+prod = prod.loc['2010-01-01':,['prod']] # setting date range
+prod = prod.astype(float)
+prod = prod.resample('B').ffill()
+prod = prod/21
+print(prod)
+#print('\n')
