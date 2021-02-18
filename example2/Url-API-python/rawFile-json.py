1b4bcb1a607624c298a925d7987b107305fc5fbf
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-18 19:50:58 +0800

    Update rawFile-json.py

--- example2/Url-API-python/rawFile-json.py
+++ example2/Url-API-python/rawFile-json.py
@@ -1,5 +1,20 @@
 import requests
 
-link = "http://api.eia.gov/series/?series_id=series_id&api_key=xxxxx"
+# XML format
+link = "http://api.eia.gov/series/?series_id=NG.N9010US2.M&api_key=xxxxx&out=xml"
 f = requests.get(link)
 print(f.text)
+
+# Json format
+link = "http://api.eia.gov/series/?series_id=NG.N9010US2.M&api_key=xxxx&out=json"
+f = requests.get(link)
+print(f.text)
+
+link = "http://api.eia.gov/category/?api_key=xxxxx"
+f = requests.get(link)
+print(f.text)
+
+link = "http://api.eia.gov/series/categories/?series_id=xxxx&api_key=xxxx"
+f = requests.get(link)
+print(f.text)
+
