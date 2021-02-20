de563a64dfa8f297e822aea7ebeb50ea37257e75
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-20 13:03:57 +0800

    Update sorting.py

--- Json-API/sorting.py
+++ Json-API/sorting.py
@@ -8,6 +8,7 @@ with open("package_info.json", "r") as f:
 def install_sort(package):
     return package['analytics']['30d']
     
+data = [item for item in data if 'video' in item ['desc']]
 data.sort(key=install_sort, reverse = True)
 
 data_str = json.dumps(data, indent=2)
