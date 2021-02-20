b9faf515a043ce4a95d4bd634d4dbc882fc3207d
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-20 17:50:30 +0800

    Update marketed-production-data.py

--- example2/marketed-production-data.py
+++ example2/marketed-production-data.py
@@ -1,41 +1,63 @@
+import pandas as pd
+import eia
+
 # U.S. Natural Gas Marketed Production, Monthly
 print('\033[4mNatural Gas Marketed Production, Monthly (Million Cubic Feet)\033[0m')
+
+
 def retrieve_time_series(api, series_ID):
     """
     Return the time series dataframe, based on API and unique Series ID
     """
-    #Retrieve Data By Series ID 
+    # Retrieving Data By Series ID
     series_search = api.data_by_series(series=series_ID)
-    ##Create a pandas dataframe from the retrieved time series
-    prod = pd.DataFrame(series_search)
-    return prod
+
+    # Creating pandas dataframe from the retrieved time series
+    MarketedProduction = pd.DataFrame(series_search)
+
+    return MarketedProduction
+
 
 def main():
     """
     Run main script
     """
     try:
-      #Create EIA API using your specific API key
-      api_key = "xxxxx"
-      api = eia.API(api_key)
-      #Declare desired series ID
-      series_ID='NG.N9050US2.M'
-      prod = retrieve_time_series(api, series_ID)
-      #Print the returned dataframe df
-      print(type(prod))
-      return prod;
-    except Exception as e:
-      print("error", e)
-      return pd.DataFrame(columns=None)
+        # Create EIA API using your specific API key
+        api_key = "ad819ee5a69e69390eadf300fa168fa8"
+        api = eia.API(api_key)
 
-prod = main()
-prod = prod.rename({'U.S. Natural Gas Marketed Production, Monthly (Million Cubic Feet)': 'prod'}, axis = 'columns')
-prod = prod.reset_index()
-prod['Date']= pd.to_datetime(prod['index']) 
-prod.set_index('Date', inplace=True) # setting index column
-prod = prod.loc['2010-01-01':,['prod']] # setting date range
-prod = prod.astype(float)
-prod = prod.resample('B').ffill()
-prod = prod/21
-print(prod)
-#print('\n')
+        # series ID
+        series_ID = 'NG.N9050US2.M'
+
+        MarketedProduction = retrieve_time_series(api, series_ID)
+
+        # Print the returned dataframe df
+        print(type(MarketedProduction))
+        return MarketedProduction
+
+    except Exception as e:
+        print("error", e)
+        return pd.DataFrame(columns=None)
+
+
+MarketedProduction = main()
+
+MarketedProduction = MarketedProduction.rename(
+    {'U.S. Natural Gas Marketed Production, Monthly (Million Cubic Feet)': 'MarketedProductuon'}, axis='columns')
+
+MarketedProduction = MarketedProduction.reset_index()
+
+MarketedProduction['Date'] = pd.to_datetime(MarketedProduction['index'])
+
+MarketedProduction.set_index('Date', inplace=True)  # setting index column
+# setting date range
+MarketedProduction = MarketedProduction.loc['2010-01-01':, ['prod']]
+
+prod = MarketedProduction.astype(float)
+
+MarketedProduction = MarketedProduction.resample('B').ffill()
+
+MarketedProduction = MarketedProduction/21
+
+print(MarketedProduction)
