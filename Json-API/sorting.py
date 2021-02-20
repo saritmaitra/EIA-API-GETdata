d74aa681ad743e871301b08c3ab8ed4263655d06
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-20 12:59:56 +0800

    Create sorting.py

---
+++ Json-API/sorting.py
@@ -0,0 +1,15 @@
+import json
+
+with open("package_info.json", "r") as f:
+    data = json.load(f)
+
+#print(data)
+
+def install_sort(package):
+    return package['analytics']['30d']
+    
+data.sort(key=install_sort, reverse = True)
+
+data_str = json.dumps(data, indent=2)
+
+print(data_str)
