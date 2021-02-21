2a691276a894a4cb14cd77a543c0c0dd3c4a9bdd
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-21 14:10:06 +0800

    Update webAPI-1.py

--- example2/Url-API-python/webAPI-1.py
+++ example2/Url-API-python/webAPI-1.py
@@ -1,10 +1,20 @@
 import sys
-import requests
+import requests  # requests library handles SSL certificate verification for you
 import os
 import math
+from string import Template
+
+"""f-strings are interesting; but as flexibility increases, so does the potential for exploits
+We have used string format here which is a bit cumbersome because it requires an import statement and it is less flexible with types. 
+It also doesn’t evaluate Python statements the way f-strings do. 
+These constraints make template strings an excellent choice when dealing with user input."""
 
 # print(sys.version)
 print(sys.executable)
+
+greeting_template = Template("Hello World, my name is $name.")
+greeting = greeting_template.substitute(name="Sarit Maitra")
+print(greeting)
 
 
 def greet(who_to_greet):
